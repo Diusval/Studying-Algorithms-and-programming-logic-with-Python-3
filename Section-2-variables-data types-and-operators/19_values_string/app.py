1b12ccfc4ea010b94014c8cc867008e1719da60d
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-08 14:57:12 -0300

    Add files via upload

---
+++ Section-2-variables-data types-and-operators/19_values_string/app.py
@@ -0,0 +1,11 @@
+profession = "programmer"
+
+print(profession)
+print("Programmer")
+
+phrase = "With great power comes great responsibility"
+
+print(len(profession))
+print(len("programmer"))
+
+print(len(phrase))
