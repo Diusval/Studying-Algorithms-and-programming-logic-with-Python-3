d1946da3d7d88b1ba9fce7d90485ceeaae927518
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-09 21:50:43 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção02-variáveis-tipos-e-operadores-de-dados/25_exercicio_11/app.py
@@ -0,0 +1,16 @@
+#1-Crie uma string dinâmica exibindo o seu nome e a sua idade;
+
+#2-Crie uma outra string dinâmica exibindo uma quandidade de dinheiro que você
+#possui (precisa  por os centavos, ou seja, utilizar float);
+
+#1-
+nome = "Diusval"
+idade = 20
+
+print("Olá, me chamo %s e tenho %d anos." % (nome, idade))
+
+#2-
+nome = "Betina"
+dinheiro = 1000000.00
+
+print("Olá, eu me chamo %s e tenho %.2f de patrimônio acumulado." % (nome, dinheiro))
