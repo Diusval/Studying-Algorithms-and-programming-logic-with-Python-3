d3600669dcd6a9531ba6863e2cfa6a0d7ac8efa1
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-15 10:32:15 -0300

    Add files via upload

---
+++ PORTUGUESE/seção03-estrutura-de-condição/1_troca_de_pneu/app.py
@@ -0,0 +1,4 @@
+#crie um alogoritmo para trocar o pneu de um carro
+#Obs: escreva numa folha de papel
+
+#pego as ferramentas, tiro os parafsos, retiro o pneu antigo, pego o pneu novo, coloco o pneu novo, parafuso o pneu novo e guardo as ferramentas.
