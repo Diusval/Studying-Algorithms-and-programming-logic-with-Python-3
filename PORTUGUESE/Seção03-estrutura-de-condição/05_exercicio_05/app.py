70c20dafec30ab403b4f2371fb4a2f912f108c51
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-15 12:07:11 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção03-estrutura-de-condição/05_exercicio_05/app.py
@@ -0,0 +1,11 @@
+#Escreva um programa que lê dois números
+#depois imprima o maior deles;
+
+n1 = float(input("Digite um número: "))
+n2 = float(input("Digite outro o número: "))
+
+if n1 > n2:
+  print("O primeiro número: %.0f é maior do que %.0f." % (n1, n2))
+
+if n2 > n1:
+  print("O segundo número: %.0f é maior do que %.0f." % (n2, n1))
