2ed9e30ceab8f4db4d7cb2b2bd6ce2893988778e
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-16 22:08:08 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção03-estrutura-de-condição/11_elif/app.py
@@ -0,0 +1,31 @@
+import nacl
+
+
+nome = input("Digite o seu nome: ")
+
+if nome == "Diusval":
+  print("Ola, voce e um admin!")
+
+  if 5 > 10:
+    print("testando")
+  elif 10 < 5:
+    print("caiu aqui!")
+    
+elif nome == "Luiza":
+  print("Ola, voce e a produtora de conteudo!")
+else:
+  print("voce e um usuario comum!")
+
+
+  numero = int(input("Digite um numero: "))
+
+  if numero > 0 and numero <= 5:
+    print("Maior que 0.")
+  elif numero > 5 and numero <= 10:
+    print("Maior que 5.")
+  elif numero > 10 and numero <= 20:
+    print("Maior que 10.")
+  elif numero > 20:
+    print("Maior que 20.")
+  else:
+    print("Numero negativo.")
