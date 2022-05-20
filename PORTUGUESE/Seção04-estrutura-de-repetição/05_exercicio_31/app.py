09bbadf5d34e513c99689b4299379dd27db1e93a
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-19 22:54:56 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção04-estrutura-de-repetição/05_exercicio_31/app.py
@@ -0,0 +1,18 @@
+#Crie um programa que imprime todos os numeros pares de 1 a 50;
+
+np = 2
+
+while np <= 50:
+  print(np)
+  np = np + 2
+
+#########
+print("")
+#########")
+
+np2 = 1
+
+while np2 <= 50:
+  if np2 % 2 == 0:
+    print(np2)
+  np2 = np2 + 1
