b2588d729bb19a12b00ac9c49555f2e260aff9ef
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-22 11:46:46 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção05-lista/1_listas/app.py
@@ -0,0 +1,16 @@
+from ast import boolop
+from xmlrpc.client import boolean
+
+
+lista = []
+print(lista)
+print(type(lista))
+
+numeros = [1, 2, 3, 4, 5]
+print(numeros)
+
+nomes = ["Diusval", "Luiza", "Irineu"]
+print(nomes)
+
+boolean = [True, False, True]
+print(boolean)
