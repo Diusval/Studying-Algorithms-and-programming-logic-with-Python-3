3da3a3b911bbaad1168cb7c110557cd2e276e63f
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-23 17:58:46 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção05-lista/8_fatiando_listas/app.py
@@ -0,0 +1,12 @@
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+print(lista)
+
+nova_lista = lista[4:8]
+print(nova_lista)
+
+nova_lista = lista[6:]
+print(nova_lista)
+
+outra_lista = lista[:4]
+print(outra_lista)
