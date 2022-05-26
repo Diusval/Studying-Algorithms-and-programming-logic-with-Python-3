e15a3b3192d88875b3ebdd5d851f66e320d6b1e7
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-25 21:46:59 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção05-lista/02_acessando_modificando/app.py
@@ -0,0 +1,16 @@
+lista = [1, 2, 3, 4, 5]
+
+print(lista[0])
+print(lista[3])
+
+nomes = ["João","Raimundo", "Ediberto"]
+
+nomes[0] = "Diusval"
+
+print(nomes)
+
+print("o meu nome é %s." %nomes[0])
+
+nome = nomes[0]
+
+print(nome)
