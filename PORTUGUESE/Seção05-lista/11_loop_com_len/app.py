81a195d164676289382275b32612391c2f5bd26e
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-25 21:45:12 -0300

    Delete PORTUGUESE/Seção05-lista directory

--- PORTUGUESE/Seção05-lista/11_loop_com_len/app.py
+++
@@ -1,7 +0,0 @@
-lista = [1,2,3,4,5,6,7,8,9,10,15,184,575,22]
-
-i = 0
-
-while i < len(lista):
-  print(lista[i])
-  i = i + 1
--- PORTUGUESE/Seção05-lista/3_percorrer_lista/app.py
+++
@@ -1,7 +0,0 @@
-lista = ["Maça", "Uva", "Mamão", "Goiaba"]
-
-i = 0
-
-while i < 4:
-  print(lista[i])
-  i = i + 1
