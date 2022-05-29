47b2fb0bf8744b59a165a2cef6c5571352168903
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-29 17:55:02 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção05-lista/35_iterando_tuplas/app.py
@@ -0,0 +1,13 @@
+tupla = ("Diusval", "Luiza", "Irineu")
+
+for nome in tupla:
+  print(nome)
+
+  if nome == "Diusval":
+    print("Parabéns Diusval!")
+
+i = 0
+
+while i < len(tupla):
+  print(tupla[i])
+  i = i + 1
