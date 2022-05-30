df14422d576003d2eabbf5207a025394d4b24a7a
Diusval <79423948+Diusval@users.noreply.github.com>
2022-05-30 20:49:30 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção06-função/01_primeira_funcao/app.py
@@ -0,0 +1,17 @@
+def digaoi(nome):
+  print("Ola, %s!" % nome)
+
+digaoi("Diusval")
+
+digaoi("Luiza")
+
+if 10 > 5:
+  digaoi("Irineu")
+
+digaoi("Irineudo")
+
+def digaola():
+  print("ola, mundo!")
+  print("Ola, Diusval!")
+
+digaola()
