7d631fbd0261ed05c30493375a6ddaef50afe215
Diusval <79423948+Diusval@users.noreply.github.com>
2022-06-02 21:45:10 -0300

    Update app.py

--- PORTUGUESE/Seção06-função/05_exercicio_52/app.py
+++ PORTUGUESE/Seção06-função/05_exercicio_52/app.py
@@ -1,19 +1,19 @@
-#Escreva uma funcao que recebe um dado em texto;
-#Se esse dado tem de 20 caracteres, retorne que e um texto longo;
-#Se tem menos, retorne que e um texto curto;
+#Escreva uma função que recebe um dado em texto;
+#Se esse dado tem mais de 20 caracteres, retorne que é um texto longo;
+#Se tem menos, retorne que é um texto curto;
 
 def contador(texto):
   r = ""
 
   if len(texto) > 20:
-    print("Tem mais que 20 caracteres esse texto e longo :)")
+    print("Tem mais que 20 caracteres esse texto é longo :)")
   
   elif len(texto) < 20:
-    print("Tem menos de 20 caracteres esse texto e muito curto :(")
+    print("Tem menos de 20 caracteres esse texto é muito curto :(")
 
   return r
 
-pri = contador("Ola tudo bem eu me chamo Diusval")
+pri = contador("Olá tudo bem eu me chamo Diusval")
 print(pri)
 
 seg = contador("oi")
