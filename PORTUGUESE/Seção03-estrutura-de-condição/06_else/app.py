d7430ae58c22a92814581c6021cece35f9b90290
Diusval <79423948+Diusval@users.noreply.github.com>
2022-06-22 22:30:47 -0300

    Delete PORTUGUESE/Seção03-estrutura-de-condição directory

--- PORTUGUESE/Seção03-estrutura-de-condição/06_else/app.py
+++
@@ -1,9 +0,0 @@
-poupanca = 500
-
-saque = float(input("Quanto deseja sacar: "))
-
-if saque <= poupanca:
-  print("Você sacou R$%.2f." % saque)
-else:
-  print("Você não tem saldo para sacar R$%.2f." % saque)
-  print("Sua poupança tem no momento R$%.2f." % poupanca)
