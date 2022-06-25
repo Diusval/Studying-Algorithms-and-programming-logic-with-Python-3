b851e6f23a74cf13e20939cbd4873108d9996e1c
Diusval <79423948+Diusval@users.noreply.github.com>
2022-06-25 12:36:38 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção08-Manipulação-de-string/07_split/app.py
@@ -0,0 +1,10 @@
+itens = "Maçaneta, Porta, Motor, Teto Solar, Janela, Embreagem"
+
+#Para não ficar o espaço na lista irei colocar um espaço no split.
+lista = itens.split(", ")
+
+print(lista)
+
+teste = "Item1@Item2@Item3"
+
+print(teste.split("@"))
