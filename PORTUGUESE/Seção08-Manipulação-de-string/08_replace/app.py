5715b3e253e4785ae53b3fe064fa8d3bc5fd1d96
Diusval <79423948+Diusval@users.noreply.github.com>
2022-06-25 14:16:30 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção08-Manipulação-de-string/08_replace/app.py
@@ -0,0 +1,9 @@
+#REPLACE
+frase = "O rato roeu a roupa do rei de Roma."
+
+print(frase.replace("rato", "leão"))
+
+#Aqui eu limitei a quantidade de vezes que o REPLACE iria funcionar e ele só funcionou 1 vez.
+frase2 = "Vou testar o replace em duas palavras testar."
+
+print(frase2.replace("testar","trocou", 1))
