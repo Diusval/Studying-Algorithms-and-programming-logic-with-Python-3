011099a8f8d8397cbe14c92e7f55f5f2c0c22a2b
Diusval <79423948+Diusval@users.noreply.github.com>
2022-06-25 10:01:34 -0300

    Add files via upload

---
+++ PORTUGUESE/Seção08-Manipulação-de-string/03_maiuscula_minuscula/app.py
@@ -0,0 +1,15 @@
+palavra = "Diusval"
+
+print(palavra.lower())
+
+teste = "O FRASE EM CAPS"
+
+print(teste.lower())
+
+min = "tudo em minusculo"
+
+print(min.upper())
+
+min2 = min.upper()
+
+print(min2)
