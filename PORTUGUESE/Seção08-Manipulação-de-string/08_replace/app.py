071a13c74a12b55b94156f414481923c3bd3c95c
Diusval <79423948+Diusval@users.noreply.github.com>
2022-06-25 14:18:05 -0300

    Update app.py

--- PORTUGUESE/Seção08-Manipulação-de-string/08_replace/app.py
+++ PORTUGUESE/Seção08-Manipulação-de-string/08_replace/app.py
@@ -3,7 +3,7 @@ frase = "O rato roeu a roupa do rei de Roma."
 
 print(frase.replace("rato", "leão"))
 
-#Aqui eu limitei a quantidade de vezes que o REPLACE iria funcionar e ele só funcionou 1 vez.
-frase2 = "Vou testar o replace em duas palavras testar."
+#Aqui eu limitei a quantidade de vezes que o REPLACE iria funcionar e ele só funcionou 2 vezes.
+frase2 = "Vou testar o replace em duas palavras testar testar testar testar."
 
-print(frase2.replace("testar","trocou", 1))
+print(frase2.replace("testar","trocou", 2))
